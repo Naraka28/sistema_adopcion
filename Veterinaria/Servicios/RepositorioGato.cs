cfa70fc98cdfee5570e66c8bd71194d47d8cc602
Naraka28 <eildan28@hotmail.com>
2025-09-23 14:18:15 -0700

    added cards, validations, repositories and more shit

---
+++ Veterinaria/Servicios/RepositorioGato.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using Veterinaria.Models;
+namespace Veterinaria.Servicios
+{
+	public interface IRepositorioGato
+	{
+		List<Gatos> ObtenerGatos();
+	}
+
+
+	public class RepositorioGato: IRepositorioGato
+	{
+		public RepositorioGato()
+		{
+		}
+
+        private readonly string _filePath = "Data/Gatos.json";
+
+        public List<Gatos> ObtenerGatos()
+        {
+            if (!File.Exists(_filePath)) return new List<Gatos>();
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Gatos>>(json) ?? new List<Gatos>();
+        }
+    }
+}
+
---
+++ Veterinaria/Validaciones/NoCaracteresEspeciales.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+namespace Veterinaria.Validaciones
+{
+    public class NoCaracteresEspeciales : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+
+                return base.IsValid(value, validationContext);
+            }
+
+            string pattern = @"^[\p{L}\s]+$";
+
+            if (!Regex.IsMatch(value.ToString(), pattern))
+            {
+                return new ValidationResult("Tu nombre no debe tener caracteres especiales");
+            }
+
+
+            return ValidationResult.Success;
+        }
+        public NoCaracteresEspeciales()
+        {
+        }
+    }
+}
+
