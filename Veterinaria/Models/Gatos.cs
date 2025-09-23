972b8ee3f75fa7052a3dd3e45f74bc8977501a82
Naraka28 <eildan28@hotmail.com>
2025-09-22 22:47:12 -0700

    added cats photos

---
+++ Veterinaria/Models/Gatos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Veterinaria.Models
+{
+	public class Gatos
+	{
+		public string Nombre { get; set; }
+		public string Sexo { get; set; }
+		public string Raza { get; set; }
+		public string Edad { get; set; }
+		public string Foto { get; set; }
+		public Gatos()
+		{
+		}
+	}
+}
+
